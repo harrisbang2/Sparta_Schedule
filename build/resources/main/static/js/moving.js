be7e97e96c64abaaad1585803026cabbdb96605a
harrisbang2 <84154173+harrisbang2@users.noreply.github.com>
2024-02-06 15:36:41 +0900

    comments added

---
+++ build/resources/main/static/js/moving.js
@@ -0,0 +1,22 @@
+let host = 'http://' + window.location.host;
+
+// $(document).ready(function () {
+//     const auth = getToken();
+//     if(auth === '') {
+//         window.location.href = host + "/api/user/login-page";
+//     } else {
+//         $('#login-true').show();
+//         $('#login-false').hide();
+//     }
+// })
+
+function searchPage() {
+    // 토큰 삭제
+    //Cookies.remove('Authorization', { path: '/' });
+    window.location.href = host + "/api/user/search-page";
+}
+function homePage() {
+    // 토큰 삭제
+    //Cookies.remove('Authorization', { path: '/' });
+    window.location.href = host + "/";
+}
