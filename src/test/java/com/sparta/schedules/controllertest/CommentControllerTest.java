5d306b5cc82a1b30e84c1f9744224fc51aeb4dfe
sehun <harrisbang2@gmail.com>
2024-02-22 14:24:47 +0900

    # test     : comment service 완료

--- src/test/java/com/sparta/schedules/controllertest/CommentControllerTest.java
+++ src/test/java/com/sparta/schedules/controllertest/CommentControllerTest.java
@@ -2,15 +2,12 @@ package com.sparta.schedules.controllertest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sparta.schedules.config.WebSecurityConfig;
-import com.sparta.schedules.controller.UserController;
 import com.sparta.schedules.dto.CommentRequestDto;
-import com.sparta.schedules.dto.ScheduleRequestDto;
 import com.sparta.schedules.entity.User;
 import com.sparta.schedules.entity.UserRoleEnum;
 import com.sparta.schedules.security.UserDetailsImpl;
 import com.sparta.schedules.service.CommentService;
 import com.sparta.schedules.service.ScheduleServices;
-import com.sparta.schedules.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -26,7 +23,6 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.security.Principal;
-import java.time.LocalDate;
 
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -119,11 +115,11 @@ public class CommentControllerTest {
         requestDto.setComment(contents);
         requestDto.setSchedule_id(1L);
 
-        String postform  = objectMapper.writeValueAsString(requestDto);
+        String platform  = objectMapper.writeValueAsString(requestDto);
 
         // when - then
         this.mvc.perform(put("/api/comment/1")
-                        .content(postform)
+                        .content(platform)
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON)
                         .principal(mockPrincipal)
