d1004c7f4acc50599208711c44e9468fc9e2ea21
sehun <harrisbang2@gmail.com>
2024-03-14 14:53:48 +0900

    userprofile projection  추가

--- src/main/java/com/sparta/schedules/repository/UserRepository.java
+++ src/main/java/com/sparta/schedules/repository/UserRepository.java
@@ -1,13 +1,14 @@
 package com.sparta.schedules.repository;
 
 import com.sparta.schedules.entity.User;
+import com.sparta.schedules.repository.projectionInterface.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
 
-    Optional<User> findByEmail(String email);
+    Optional<UserProfile> findByEmail(String email);
 
     Optional<User> findByUsername(String username);
 }
