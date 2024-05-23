cfcdc9c5087b3d06544c10243e59cd1696d16739
harrisbang2 <harrisbang2@gmail.com>
2024-05-23 23:34:50 +0900

    refactor : querydsl 추가

---
+++ src/main/generated/com/sparta/schedules/domain/user/entity/QUser.java
@@ -0,0 +1,48 @@
+package com.sparta.schedules.domain.user.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QUser is a Querydsl query type for User
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QUser extends EntityPathBase<User> {
+
+    private static final long serialVersionUID = 2005275909L;
+
+    public static final QUser user = new QUser("user");
+
+    public final StringPath email = createString("email");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath password = createString("password");
+
+    public final EnumPath<UserRoleEnum> role = createEnum("role", UserRoleEnum.class);
+
+    public final ListPath<com.sparta.schedules.domain.schedule.entity.Schedule, com.sparta.schedules.domain.schedule.entity.QSchedule> schedules = this.<com.sparta.schedules.domain.schedule.entity.Schedule, com.sparta.schedules.domain.schedule.entity.QSchedule>createList("schedules", com.sparta.schedules.domain.schedule.entity.Schedule.class, com.sparta.schedules.domain.schedule.entity.QSchedule.class, PathInits.DIRECT2);
+
+    public final StringPath username = createString("username");
+
+    public QUser(String variable) {
+        super(User.class, forVariable(variable));
+    }
+
+    public QUser(Path<? extends User> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QUser(PathMetadata metadata) {
+        super(User.class, metadata);
+    }
+
+}
+
