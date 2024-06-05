2a665c1cc257b3c7a7c75ba98c64ec69911190e1
harrisbang2 <harrisbang2@gmail.com>
2024-06-05 15:59:09 +0900

    refactor(user) : Schedules repo 정리

--- src/main/java/com/sparta/schedules/domain/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/sparta/schedules/domain/schedule/repository/ScheduleRepository.java
@@ -1,12 +1,7 @@
 package com.sparta.schedules.domain.schedule.repository;
 
-import com.sparta.schedules.domain.schedule.dto.ScheduleResponseDto;
 import com.sparta.schedules.domain.schedule.entity.Schedule;
-import com.sparta.schedules.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.time.LocalDate;
-import java.util.List;
 
 public interface ScheduleRepository extends JpaRepository<Schedule,Long>,ScheduleRepositoryCustom {
 
